9fdd352560f0cf0b0499607a0eb153311bfd2a49
Vega092 <aleubil02@gmail.com>
2024-04-30 10:07:43 +0900

    [Feat] 게임 틀 작성

--- UnityClient_A_2024_01/Assets/Scripts/StoryGame/Enums.cs
+++ UnityClient_A_2024_01/Assets/Scripts/StoryGame/Enums.cs
@@ -2,18 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace STORYGAME     //이름 충돌 방지
+namespace STORYGAME //이름 충돌 방지
 {
-    public class Enums              //열거형 타입
+    public class Enums      //열거형 타입 
     {
-        public enum StoryType           //스토리 타입
+        public enum StoryType      //스토리 타입
         {
             MAIN,
             SUB,
             SERIAL
         }
 
-        public enum EvenType            //이벤트 발생시 체크
+        public enum EvenType        //이벤트 발생시 체크
         {
             NONE,
             GoToBattle = 100,
@@ -25,14 +25,14 @@ namespace STORYGAME     //이름 충돌 방지
             CheckCHA
         }
 
-        public enum ResultType          //이벤트 결과 열거
+        public enum ResultType       //이벤트 결과 열거
         {
             ChangeHp,
             ChangeSp,
             AddExperience,
             GoToShop,
             GoToNextStory,
-            GoToRansdeomStory,
+            GoToRandeomStory,
             GoToEnding
         }
     }
@@ -40,6 +40,7 @@ namespace STORYGAME     //이름 충돌 방지
     [System.Serializable]
     public class Stats
     {
+        //체력과 정신력
         public int hpPoint;
         public int spPoint;
 
@@ -47,13 +48,13 @@ namespace STORYGAME     //이름 충돌 방지
         public int currentSpPoint;
         public int currentXpPoint;
 
-        //기본 스텟 설정
-        public int strength;            //STR
-        public int dexterity;           //DEX
-        public int consitiution;       //CON
-        public int intelligence;       //INT
-        public int wisdom;             //WIS
-        public int charisma;          //CHA
+        //기본 스텟 설정 (D&D)
+        public int strength;        //STR
+        public int dexterity;       //DEX
+        public int consitiution;    //CON
+        public int Intelligence;    //INT
+        public int wisdom;          //WIS
+        public int charisma;        //CHA
     }
 
 }
---
+++ UnityClient_A_2024_01/Assets/Scripts/StoryGame/StoryModel.cs
@@ -0,0 +1,77 @@
+using STORYGAME;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static STORYGAME.StoryTableObject;
+
+
+[CreateAssetMenu(fileName = " New Story" , menuName = "ScriptableObjects/StoryModel")]
+public class StoryModel : MonoBehaviour
+{
+    public int storyNumber;                 //스토리 번호
+    public Texture2D MainImage;       //스토리 보여줄 이미지 텍스쳐
+
+    public enum STORYTYPE
+    {
+        MAIN,
+        SUB,
+        SERIAL
+    }
+
+    public STORYTYPE storytype;
+    public bool storyDone;
+
+    [TextArea(10, 10)]
+    public string storyText;                    //메인 스토리
+
+    public Option[] options;                    //선택지 배열
+
+    [System.Serializable]
+    public class Option
+    {
+        public string optionText;
+        public string buttonText;
+        public EventCheck eventCheck;
+    }
+
+    [System.Serializable]
+    public class EventCheck
+    {
+        public int checkValue;
+        public enum EventType : int
+        {
+            NINE,
+            GoToBattle,
+            CheckSTR,
+            CheckDex,
+            CheckCON,
+            CheckINT,
+            CheckWIS,
+            CheckCHA
+        }
+
+        public EventCheck eventType;
+
+        public Result[] successResult;
+        public Result[] failedResult;
+    }
+
+    [System.Serializable]
+    public class Result
+    {
+        public enum ResultType : int
+        {
+            ChangeHP,
+            ChangeSP,
+            AddExperince,
+            GoToShop,
+            GoToNextStory,
+            GoToRandomStory,
+            GoToEnding
+        }
+
+        public ResultType resultType;           //결과값 타입
+        public int value;                                 //변화 수치 입력
+        public Stats stats;                             //해당 스택 변화 수치
+    }
+}
